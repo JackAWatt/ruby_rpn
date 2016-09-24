3eac646ef7760fdec9acebe71fcad9f6705cad26
Jack Watt <jsw.watt@gmail.com>
2016-09-24 14:03:38 -0300

    change where th
    Changes where the operands are cast to float and where the operator is cast to symbol

--- rpn.rb
+++ rpn.rb
@@ -8,12 +8,10 @@ class Fixnum
 		Float(self) != nil rescue false
 	end
 end
-# 
-i = 0
 def compute(a, operator, b)
-	a.send(operator, b)	
+	a.to_f.send(operator.to_sym, b.to_f)	
 end
-
+i = 0
 input = gets.strip
 stack = input.gsub(/\s+/m, ' ').strip.split(" ")
 if stack.length < 3
@@ -21,7 +19,7 @@ if stack.length < 3
 else
 	while stack.length > 1
 		if !stack[i].numeric?
-			stack[i - 2] = compute(stack[i-2].to_f,stack[i].to_sym,stack[i-1].to_f)
+			stack[i-2] = compute(stack[i-2],stack[i],stack[i-1])
 			stack.delete_at(i)
 			stack.delete_at(i-1)
 			i = 0
