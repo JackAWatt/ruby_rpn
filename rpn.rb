e069c5d77da4b9cb19cc5c5c65d8b126f2e2eefd
Jack Watt <jsw.watt@gmail.com>
2016-09-24 17:28:13 -0300

    continue to separate concerns into functions

--- rpn.rb
+++ rpn.rb
@@ -14,12 +14,23 @@ def compute(a, operator, b)
 	a.to_f.send(operator.to_sym, b.to_f)	
 end
 
+def return_stack(stack, i)
+	stack.delete_at(i)
+	stack.delete_at(i-1)
+	return stack
+	
+end
+
+def compute_value(stack, i)
+	compute(stack[i-2],stack[i],stack[i-1])
+end
+
 def compute_stack(stack, i)
 	if stack.length > 1
 		if !stack[i].numeric?
-			stack[i-2] = compute(stack[i-2],stack[i],stack[i-1])
-			stack.delete_at(i)
-			stack.delete_at(i-1)
+			stack[i-2] = compute_value(stack, i)
+			stack = return_stack(stack, i)
+
 			compute_stack(stack,0)
 		else
 			compute_stack(stack,i + 1)
