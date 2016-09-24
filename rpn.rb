ae9ce8d1151ae0099ec38c370d3cf466976218c3
Jack Watt <jsw.watt@gmail.com>
2016-09-24 16:56:34 -0300

    Update rpn.rb
    use recursion instead of mutation

--- rpn.rb
+++ rpn.rb
@@ -14,16 +14,15 @@ def compute(a, operator, b)
 	a.to_f.send(operator.to_sym, b.to_f)	
 end
 
-def compute_stack(stack)
-	i = 0
+def compute_stack(stack, i)
 	while stack.length > 1
 		if !stack[i].numeric?
 			stack[i-2] = compute(stack[i-2],stack[i],stack[i-1])
 			stack.delete_at(i)
 			stack.delete_at(i-1)
-			i = 0
+			compute_stack(stack,0)
 		else
-			i = i + 1
+			compute_stack(stack,i + 1)
 		end
 	end
 end
@@ -34,6 +33,6 @@ end
 
 while :input
 	stack = get_stack
-	compute_stack(stack)
+	compute_stack(stack, 0)
 	p stack
 end
