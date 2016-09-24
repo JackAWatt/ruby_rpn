d97f73bc5f16c30c79ef575eb80eee06e00f8809
Jack Watt <jsw.watt@gmail.com>
2016-09-24 12:45:05 -0300

    refactor spacing

--- rpn.rb
+++ rpn.rb
@@ -1,4 +1,3 @@
-
 class String
   def numeric?
     Float(self) != nil rescue false
@@ -14,23 +13,22 @@ i = 0
 def do_the_stuff(a, operator, b)
 	a.send(operator, b)	
 end
-# 1:
-	input = gets.strip
-# 2:
-	rpn_stack = input.gsub(/\s+/m, ' ').strip.split(" ")
-# 3:
-	if rpn_stack.length < 3
-		puts "not enough operators"
-	else
-		while rpn_stack.length > 1
-			if !rpn_stack[i].numeric?
-				rpn_stack[i - 2] = do_the_stuff(rpn_stack[i-2].to_i,rpn_stack[i].to_sym,rpn_stack[i-1].to_i)
-				rpn_stack.delete_at(i)
-				rpn_stack.delete_at(i-1)
-				i = 0
-			else
-				i = i + 1
-			end
+def 
+
+input = gets.strip
+rpn_stack = input.gsub(/\s+/m, ' ').strip.split(" ")
+if rpn_stack.length < 3
+	puts "not enough operators"
+else
+	while rpn_stack.length > 1
+		if !rpn_stack[i].numeric?
+			rpn_stack[i - 2] = do_the_stuff(rpn_stack[i-2].to_i,rpn_stack[i].to_sym,rpn_stack[i-1].to_i)
+			rpn_stack.delete_at(i)
+			rpn_stack.delete_at(i-1)
+			i = 0
+		else
+			i = i + 1
 		end
 	end
-	p rpn_stack
+end
+p rpn_stack
